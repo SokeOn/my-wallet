4ea25d5d9d49aaa9757e9a63084b05e1586d8eab
RubenBaquedano <rubiquedano@gmail.com>
2023-01-26 10:48:54 +0100

    SocialOnce
    a

---
+++ virtual-wallet-master/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDHJtnapp5otPksoOlFyed3FqQMHBqGos",
+    authDomain: "my-wallet-e5e22.firebaseapp.com",
+    projectId: "my-wallet-e5e22",
+    storageBucket: "my-wallet-e5e22.appspot.com",
+    messagingSenderId: "1093325461471",
+    appId: "1:1093325461471:web:a908d5d4052d04904bd66a",
+    measurementId: "G-JE2BPSZYS7"
+  }
+  
+};
